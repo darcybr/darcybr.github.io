ac3c1e18f0eb675449b998007aeb8d5c754b4282
darcybr <darcybrennanvi@gmail.com>
2026-01-21 12:06:40 -0800

    update scifi survivors

--- projects/solomongame2/SciFiSurvivors.service.worker.js
+++ projects/solomongame2/SciFiSurvivors.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768850138|799926443';
+const CACHE_VERSION = '1769024661|5058216174';
 /** @type {string} */
 const CACHE_PREFIX = 'Rogue-like-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
